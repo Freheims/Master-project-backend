dfb2549e1a77f52dfe525d293b1a842364069e44
Fredrik Heimsæter <freheims@gmail.com>
2018-04-19 16:35:25 +0200

    Fix headers

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
 
-        c.Writer.Header().Set("Content-Type", "application/json, multipart/form-data")
+        c.Writer.Header().Set("Content-Type", "application/json")
         c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, POST")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
